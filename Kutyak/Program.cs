a8e327cc46dccf2bee546b232778f99a32e24485
beketamas <tamas.beke0921@gmail.com>
2023-05-25 11:53:57 +0200

    mentes

--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+
+            //2. feladat
+            List<string> KutyaNevek = File.ReadAllLines("..//..//..//Adatok//KutyaNevek.csv").Skip(1).Select(x => x.Split(";")[1]).ToList();
+            //3. feladat
+            Console.WriteLine($"3. feladat: {KutyaNevek.Count}");
+
+
         }
     }
 }

32dc2890e769998ab64da6a1e7f65f2f64d2c7a0
beketamas <tamas.beke0921@gmail.com>
2023-05-25 12:51:43 +0200

    ebéd után

--- Kutyak/Kutyak.cs
+++ Kutyak/Kutyak.cs
@@ -8,6 +8,53 @@ namespace Kutyak
 {
     internal class Kutyak
     {
+        int id;
+        int fajta_id;
+        int nev_id;
+        int eletkor;
+        string utolso_orvosi;
+        string nev;
+        string eredeti_Nev;
+        string kutyaNev;
 
+
+        public Kutyak(string sor)
+        {
+            string[] tomb = sor.Split(";");
+            this.id = int.Parse(tomb[0]);
+            this.fajta_id = int.Parse(tomb[1]);
+            this.nev_id = int.Parse(tomb[2]);
+            this.eletkor = int.Parse(tomb[3]);
+            this.utolso_orvosi = tomb[4];
+        }
+
+        public Kutyak(string sor, bool c) 
+        {
+            if (c == true)
+            {
+                string[] tomb = sor.Split(";");
+                this.id = int.Parse(tomb[0]);
+                this.nev = tomb[1];
+                this.eredeti_Nev = tomb[2];
+            }
+            else if(c == false)
+            {
+                string[] tomb = sor.Split(";");
+                this.id = int.Parse(tomb[0]);
+                this.kutyaNev = tomb[1];
+            }
+            
+        }
+
+
+
+        public int Id { get => id;}
+        public int Fajta_id { get => fajta_id;}
+        public int Nev_id { get => nev_id;}
+        public int Eletkor { get => eletkor;}
+        public string Utolso_orvosi { get => utolso_orvosi;}
+        public string Nev { get => nev;}
+        public string Eredeti_Nev { get => eredeti_Nev;}
+        public string KutyaNev { get => kutyaNev;}
     }
 }
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -6,9 +6,27 @@
         {
 
             //2. feladat
-            List<string> KutyaNevek = File.ReadAllLines("..//..//..//Adatok//KutyaNevek.csv").Skip(1).Select(x => x.Split(";")[1]).ToList();
+            List<Kutyak> KutyaNevek = File.ReadAllLines("..//..//..//Adatok//KutyaNevek.csv").Skip(1).Select(x => new Kutyak(x,false)).ToList();
             //3. feladat
             Console.WriteLine($"3. feladat: {KutyaNevek.Count}");
+
+            //6. feladat
+            List<Kutyak> kutyak = File.ReadAllLines("..//..//..//Adatok//Kutyak.csv").Skip(1).Select(x => new Kutyak(x)).ToList();
+            Console.WriteLine($"6. feladat: {kutyak.Average(x => x.Eletkor):f2}");
+
+            List<Kutyak> kutyaFajtak = File.ReadAllLines("..//..//..//Adatok//KutyaFajtak.csv").Skip(1).Select(x => new Kutyak(x,true)).ToList();
+
+            //7. feladat
+            int eletkor_id = kutyak.OrderByDescending(x => x.Eletkor).ToList()[0].Fajta_id;
+            int id = kutyak.OrderByDescending(x => x.Eletkor).ToList()[0].Nev_id;
+            string fajta = kutyaFajtak.Where(x => x.Id == eletkor_id).ToList()[0].Nev;
+            string nev = KutyaNevek.Where(x => x.Id == id).ToList()[0].KutyaNev;
+            Console.WriteLine($"7. feladat: Legidősebb kutya neve és fajtája: {nev}, {fajta}");
+
+            //8. feladat
+            var id_lista = kutyak.Count(x => x.Utolso_orvosi == "2018.01.10");
+
+
 
 
         }
